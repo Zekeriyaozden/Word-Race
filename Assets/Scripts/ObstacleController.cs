65945d0e36baddd0e14eafb07782850891203369
Zekeriyaozden <zekeriyaozden@icloud.com>
2022-06-24 11:15:59 +0300

    Obstacle

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -51,22 +51,20 @@ public class ObstacleController : MonoBehaviour
                         Debug.Log(tmpIsNotProtected[i]);
                         tmpIsNotProtected[i].GetComponent<LattersController>().makeObjectNotWork();
                     }
-                    for (int i = 0; i < tmpIsProtected.Count; i++)
+
+
+                    if (tmpIsProtected.Count > 0)
                     {
-                        /*gm.GetComponent<GameManager>().referanceParentPlayer.GetComponent<ParentPlayerController>()
-                            .PlayerStack.Remove(tmpIsProtected[i]);
-                        Debug.Log(tmpIsProtected[i]);*/
+                        tmpIsProtected[tmpIsProtected.Count - 1].GetComponent<LattersController>().node = gm.GetComponent<GameManager>().referanceParentPlayer.GetComponent<ParentPlayerController>()
+                            .PlayerStack[gm.GetComponent<GameManager>().referanceParentPlayer.GetComponent<ParentPlayerController>()
+                                .PlayerStack.IndexOf(tmpIsProtected[tmpIsProtected.Count - 1]) - 1];
+                    
+                        tmpIsProtected[tmpIsProtected.Count - 1].transform.position =
+                            tmpIsProtected[tmpIsProtected.Count - 1].GetComponent<LattersController>().node.transform.position +
+                            new Vector3(0, 0, 1.6f);
+                    
+                        tmpIsProtected[tmpIsProtected.Count - 1].GetComponent<LattersController>().distancer();
                     }
-                    
-                    tmpIsProtected[tmpIsProtected.Count - 1].GetComponent<LattersController>().node = gm.GetComponent<GameManager>().referanceParentPlayer.GetComponent<ParentPlayerController>()
-                        .PlayerStack[gm.GetComponent<GameManager>().referanceParentPlayer.GetComponent<ParentPlayerController>()
-                            .PlayerStack.IndexOf(tmpIsProtected[tmpIsProtected.Count - 1]) - 1];
-                    
-                    tmpIsProtected[tmpIsProtected.Count - 1].transform.position =
-                        tmpIsProtected[tmpIsProtected.Count - 1].GetComponent<LattersController>().node.transform.position +
-                        new Vector3(0, 0, 1.6f);
-                    
-                    tmpIsProtected[tmpIsProtected.Count - 1].GetComponent<LattersController>().distancer();
                     
 
                     for (int i = tmpIsProtected.Count - 2; i >= 0; i--)
@@ -94,35 +92,73 @@ public class ObstacleController : MonoBehaviour
                 }
             }else if (other.gameObject.GetComponent<LattersController>().ownership == "AI")
             {
-                if (gm.GetComponent<GameManager>().referanceParentAI.GetComponent<ParentAIController>()
-                    .AIStack.Contains(other.gameObject))
+ if (gm.GetComponent<GameManager>().referanceParentPlayer.GetComponent<ParentPlayerController>()
+                    .PlayerStack.Contains(other.gameObject))
                 {
-                    index = gm.GetComponent<GameManager>().referanceParentAI.GetComponent<ParentAIController>()
-                        .AIStack.IndexOf(other.gameObject);
-                    count = gm.GetComponent<GameManager>().referanceParentAI.GetComponent<ParentAIController>()
-                        .AIStack.Count - index;
-                    for (int i = 1; i <= count; i++)
+                    index = gm.GetComponent<GameManager>().referanceParentPlayer.GetComponent<ParentPlayerController>()
+                        .PlayerStack.IndexOf(other.gameObject);
+                    count = gm.GetComponent<GameManager>().referanceParentPlayer.GetComponent<ParentPlayerController>()
+                        .PlayerStack.Count;
+                    List<GameObject> tmpIsProtected = new List<GameObject>();
+                    List<GameObject> tmpIsNotProtected = new List<GameObject>();
+                    for (int i = 0; i < count - index; i++)
                     {
-                        GameObject tmp = gm.GetComponent<GameManager>().referanceParentAI.GetComponent<ParentAIController>()
-                            .AIStack[gm.GetComponent<GameManager>().referanceParentPlayer.GetComponent<ParentAIController>()
-                                .AIStack.Count - 1];
-                        if (!tmp.GetComponent<LattersController>().isProtected)
+                        if (gm.GetComponent<GameManager>().referanceParentPlayer.GetComponent<ParentPlayerController>()
+                            .PlayerStack[count - 1 - i].GetComponent<LattersController>().isProtected)
                         {
-                            gm.GetComponent<GameManager>().referanceParentAI.GetComponent<ParentAIController>()
-                                .AIStack.Remove(tmp);
-
-                            //Giden Harflere nolacak -- şimdilik Destroy();
-                            Destroy(tmp);
+                            tmpIsProtected.Add(gm.GetComponent<GameManager>().referanceParentPlayer.GetComponent<ParentPlayerController>().PlayerStack[count - 1- i]);
                         }
                         else
                         {
-                            Debug.Log( tmp.name + " ");
+                            tmpIsNotProtected.Add(gm.GetComponent<GameManager>().referanceParentPlayer.GetComponent<ParentPlayerController>().PlayerStack[count - 1- i]);
                         }
+                        
+                        
                     }
 
+                    for (int i = 0; i < tmpIsNotProtected.Count; i++)
+                    {
+                        gm.GetComponent<GameManager>().referanceParentAI.GetComponent<ParentAIController>()
+                            .AIStack.Remove(tmpIsNotProtected[i]);
+                        Debug.Log(tmpIsNotProtected[i]);
+                        tmpIsNotProtected[i].GetComponent<LattersController>().makeObjectNotWork();
+                    }
+
+
+                    if (tmpIsProtected.Count > 0)
+                    {
+                        tmpIsProtected[tmpIsProtected.Count - 1].GetComponent<LattersController>().node = gm.GetComponent<GameManager>().referanceParentAI.GetComponent<ParentAIController>()
+                            .AIStack[gm.GetComponent<GameManager>().referanceParentAI.GetComponent<ParentAIController>()
+                                .AIStack.IndexOf(tmpIsProtected[tmpIsProtected.Count - 1]) - 1];
+                    
+                        tmpIsProtected[tmpIsProtected.Count - 1].transform.position =
+                            tmpIsProtected[tmpIsProtected.Count - 1].GetComponent<LattersController>().node.transform.position +
+                            new Vector3(0, 0, 1.6f);
+                    
+                        tmpIsProtected[tmpIsProtected.Count - 1].GetComponent<LattersController>().distancer();
+                    }
+                    
+
+                    for (int i = tmpIsProtected.Count - 2; i >= 0; i--)
+                    {
+                        Debug.Log(tmpIsProtected[i].name + "tmpIsProtected");
+                        tmpIsProtected[i].GetComponent<LattersController>().node = tmpIsProtected[i + 1];
+                        tmpIsProtected[i].transform.position =
+                            tmpIsProtected[i].GetComponent<LattersController>().node.transform.position +
+                            new Vector3(0, 0, 1.6f);
+                        tmpIsProtected[i].GetComponent<LattersController>().distancer();
+                    }
+                    
+                    
+                    
+                    
+                    
+                    Destroy(gameObject);
+                    //Destroy(gameObject);
+                    //Giden Harflere nolacak -- şimdilik Destroy();
+                    
                     count = gm.GetComponent<GameManager>().referanceParentAI.GetComponent<ParentAIController>()
                         .AIStack.Count;
-                    
                     
                 } 
             }
